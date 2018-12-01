cd474985da6736e52fde1ccf7d4af89276b0e4fe
Novikov Michael <mikhailn@123software.ru>
2018-12-01 17:05:29 +0200

    1

--- RozetkaOnePage.app/RozetkaOnePage.app.Android/CustomControls/CustomSearchBarRenderer.cs
+++ RozetkaOnePage.app/RozetkaOnePage.app.Android/CustomControls/CustomSearchBarRenderer.cs
@@ -9,9 +9,12 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using RozetkaOnePage.app.CustomControls;
+using RozetkaOnePage.app.Droid.CustomControls;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
+[assembly: ExportRenderer(typeof(CustomSearchBar), typeof(CustomSearchBarRenderer))]
 namespace RozetkaOnePage.app.Droid.CustomControls
 {
     class CustomSearchBarRenderer : SearchBarRenderer
--- RozetkaOnePage.app/RozetkaOnePage.app/App.xaml.cs
+++ RozetkaOnePage.app/RozetkaOnePage.app/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Prism;
 using Prism.Ioc;
+using RozetkaOnePage.app.Services;
 using RozetkaOnePage.app.ViewModels;
 using RozetkaOnePage.app.Views;
 using Xamarin.Forms;
@@ -30,6 +31,8 @@ namespace RozetkaOnePage.app
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<MainView, MainViewModel>();
+            containerRegistry.RegisterForNavigation<ProductDetailView, ProductDetailViewModel>();
+            containerRegistry.RegisterSingleton<IProductCellService, ProductCellService>();
         }
     }
 }
